d0bd99274264cfc0a83c7e9e1a7af6b0584dda67
techstory09 <techstoryb09@gmail.com>
2024-10-19 01:47:27 +0800

    Update ev.js

--- ev.js
+++ ev.js
@@ -16,7 +16,7 @@ app.get('/', async (req, res) => {
 
     (async () => {
         const browser = await puppeteer.connect({
-            browserWSEndpoint: `wss://production-sfo.browserless.io?&token=R3LKd3ZQhulauJc3804e9653e772e5819e2cc09ee2&--proxy-server=http://45.249.104.143:6438`,
+            browserWSEndpoint: `wss://production-sfo.browserless.io?&token=R3R84WySyko0JF656d6b5fda69aeff1cb43c1d013f&--proxy-server=http://45.249.104.143:6438`,
         });
 
         const page = await browser.newPage();
